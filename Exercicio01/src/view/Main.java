d45b05b3e0dbfc2370423d6d31cbe1e385be74cb
Lucas <lucaswar05@gmail.com>
2024-08-28 19:29:54 -0300

    Commit Ex01 - Criação de menu

--- Exercicio01/src/view/Main.java
+++ Exercicio01/src/view/Main.java
@@ -1,9 +1,31 @@
 package view;
+// Trazer bibliotecas e pacotes:
 import controller.RedesController;
+import javax.swing.JOptionPane;
 public class Main {
     public static void main(String[] args) {
+        // Instancia de classe:
         RedesController met = new RedesController();
-        met.chamadaIp();
-        met.bigTesteIP();
+        // Definir variáveis:
+        int menu = 0;
+        // Entrada de dados: 
+        while(true){
+        // Processamento de dados:
+            menu = Integer.parseInt(JOptionPane.showInputDialog("Escolha: 1-ipv4 2-teste, 9-sair"));
+            switch (menu) {
+                case 1:
+                    met.chamadaIp();
+                    break;
+                case 2:
+                    met.bigTesteIP();
+                    break;
+                case 9:
+                    break;
+                default:
+                    break;
+            }
+            if (menu == 9)
+                break;
+        }
     }
 }
