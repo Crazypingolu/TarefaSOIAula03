9ab4bfd19c9af29dd3f1ae67afd61c7db68e7118
Lucas <lucaswar05@gmail.com>
2024-08-28 18:37:52 -0300

    Commit Ex01 - Métodos Os e IP finalizados

--- Exercicio01/src/controller/RedesController.java
+++ Exercicio01/src/controller/RedesController.java
@@ -1,4 +1,70 @@
 package controller;
+// Importar bibliotecas:
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.BufferedReader;
+// Classe e processos:
 public class RedesController {
-
+    // SuperClass:
+    public RedesController(){
+        super();
+    }
+    // Chamando Os:
+    private String os(){
+        String os = System.getProperty("os.name");
+        return os;
+    }
+    // Chamada de IP:
+    public void chamadaIp(){
+        // Sistema é Windows
+        if (os().contains("Windows")){
+            try{ // tenta executar o comando de ver o IP
+                Process proc = Runtime.getRuntime().exec("IPCONFIG");
+                String adpt = "";
+                // lê os processos:
+                InputStream fluxo = proc.getInputStream();
+                InputStreamReader leitor = new InputStreamReader(fluxo);
+                BufferedReader buffer = new BufferedReader(leitor);
+                String linha = buffer.readLine();
+                // tratamento de dados:
+                System.out.println(" ");
+                while (linha != null){
+                    if (linha.contains("Adaptador")){
+                        adpt = linha;
+                    } if (linha.contains("IPv4")){
+                        System.out.println(adpt);
+                        System.out.println(linha);
+                    }
+                    linha = buffer.readLine();
+                }
+                // fechamento:
+                buffer.close();
+                leitor.close();
+                fluxo.close();
+            } catch (Exception e){ // não conseguiu
+                System.err.println(e.getMessage());
+            }
+        // Sistema é Linux
+        } else if (os().contains("Linux")){
+            try{ // tenta executar o comando de ver o IP
+                Process proc = Runtime.getRuntime().exec("ip addr");
+                // lê os processos:
+                InputStream fluxo = proc.getInputStream();
+                InputStreamReader leitor = new InputStreamReader(fluxo);
+                BufferedReader buffer = new BufferedReader(leitor);
+                String linha = buffer.readLine();
+                // tratamento de dados:
+                while (linha != null){
+                    System.out.println(linha);
+                    linha = buffer.readLine();
+                }
+                // fechamento:
+                buffer.close();
+                leitor.close();
+                fluxo.close();
+            } catch (Exception e){ // não conseguiu
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 }
--- Exercicio01/src/view/Main.java
+++ Exercicio01/src/view/Main.java
@@ -1,6 +1,8 @@
 package view;
+import controller.RedesController;
 public class Main {
     public static void main(String[] args) {
-        
+        RedesController met = new RedesController();
+        met.chamadaIp();
     }
 }
