1c1cc49329af08082e1237249f3d6cfeba0800fb
Lucas <lucaswar05@gmail.com>
2024-08-28 19:22:24 -0300

    Commit Ex01 - Método Ping conclúido

--- Exercicio01/src/controller/RedesController.java
+++ Exercicio01/src/controller/RedesController.java
@@ -68,4 +68,51 @@ public class RedesController {
             }
         }
     }
+    // Chamada de ping
+    public void bigTesteIP(){
+     // Sistema é Windows
+        if (os().contains("Windows")){
+            try{ // tenta executar o comando de ver o IP
+                Process proc = Runtime.getRuntime().exec("ping -4 -n 10 www.google.com.br");
+                // lê os processos:
+                InputStream flux = proc.getInputStream();
+                InputStreamReader leit = new InputStreamReader(flux);
+                BufferedReader buff = new BufferedReader(leit);
+                String linha = buff.readLine();
+                // tratamento de dados:
+                System.out.println(" ");
+                while (linha != null){
+                    System.out.println(linha);
+                    linha = buff.readLine();
+                }
+                // fechamento:
+                buff.close();
+                leit.close();
+                flux.close();
+            } catch (Exception e){ // não conseguiu
+                System.err.println(e.getMessage());
+            }
+        // Sistema é Linux
+        } else if (os().contains("Linux")){
+            try{ // tenta executar o comando de ver o IP
+                Process proc = Runtime.getRuntime().exec("ping -4 -c 10 www.google.com.br");
+                // lê os processos:
+                InputStream flux = proc.getInputStream();
+                InputStreamReader leit = new InputStreamReader(flux);
+                BufferedReader buff = new BufferedReader(leit);
+                String linha = buff.readLine();
+                // tratamento de dados:
+                while (linha != null){
+                    System.out.println(linha);
+                    linha = buff.readLine();
+                }
+                // fechamento:
+                buff.close();
+                leit.close();
+                flux.close();
+            } catch (Exception e){ // não conseguiu
+                System.err.println(e.getMessage());
+            }
+        }
+    }
 }
--- Exercicio01/src/view/Main.java
+++ Exercicio01/src/view/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         RedesController met = new RedesController();
         met.chamadaIp();
+        met.bigTesteIP();
     }
 }
