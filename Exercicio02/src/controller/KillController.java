123bef81f7f9fcbca527419a4d7d0a0f40830908
Lucas <lucaswar05@gmail.com>
2024-08-29 01:00:03 -0300

    Commit Ex02 - Metodo processo em linux

--- Exercicio02/src/controller/KillController.java
+++ Exercicio02/src/controller/KillController.java
@@ -36,5 +36,26 @@ public class KillController {
                 System.err.println(e.getMessage());
             }
         }
+        if (os().contains("Linux")) {
+            try {
+                Process proc = Runtime.getRuntime().exec("ps -ef");
+                // ler os processos:
+                InputStream flux = proc.getInputStream();
+                InputStreamReader read = new InputStreamReader(flux);
+                BufferedReader leitor = new BufferedReader(read);
+                String line = leitor.readLine();
+                // Tratamento:
+                while (line != null){
+                    System.out.println(line);
+                    line = leitor.readLine();
+                }
+                // Fechar tudo:
+                flux.close();
+                leitor.close();
+                read.close();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
