36fa502617bfb6fab99f2e115ad255849cfef25a
Lucas <lucaswar05@gmail.com>
2024-08-28 21:32:17 -0300

    Commit Ex01 - Menu Finalizado

--- Exercicio01/src/view/Main.java
+++ Exercicio01/src/view/Main.java
@@ -11,7 +11,11 @@ public class Main {
         // Entrada de dados: 
         while(true){
         // Processamento de dados:
-            menu = Integer.parseInt(JOptionPane.showInputDialog("Escolha: 1-ipv4 2-teste, 9-sair"));
+            menu = Integer.parseInt(JOptionPane.showInputDialog(
+                "Escolha:\n" + 
+                "[1] - Mostrar IPv4\n" +
+                "[2] - Testar conexção de IPv4\n" +
+                "[9] - Fechar programa."));
             switch (menu) {
                 case 1:
                     met.chamadaIp();
@@ -22,6 +26,7 @@ public class Main {
                 case 9:
                     break;
                 default:
+                    JOptionPane.showMessageDialog(null,"Opção inválida, digite novamente.");
                     break;
             }
             if (menu == 9)
