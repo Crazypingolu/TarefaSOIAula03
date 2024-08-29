9a4a259f3be913c19eeaef4e07d19408b03f63ca
Lucas <lucaswar05@gmail.com>
2024-08-29 01:45:22 -0300

    Commit Ex03 - Classes concluídas

---
+++ Exercicio03/src/view/Main.java
@@ -0,0 +1,8 @@
+package view;
+import controller.DistroController;
+public class Main {
+    public static void main(String[] args) {
+        // Instanciar classes:
+        DistroController met = new DistroController();
+    }
+}
