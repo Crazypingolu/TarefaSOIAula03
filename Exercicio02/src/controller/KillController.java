fd5aad47b03650e556b90945c68e22334e6bdd80
Lucas <lucaswar05@gmail.com>
2024-08-29 00:53:52 -0300

    Commit Ex02 - Metodo processo e sua chamada

--- Exercicio02/src/controller/KillController.java
+++ Exercicio02/src/controller/KillController.java
@@ -1,7 +1,40 @@
 package controller;
-
+// Importar bibliotecas:
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.BufferedReader;
+// Classe e processos:
 public class KillController {
     public KillController(){
         super();
+    }
+    // Classe pra achar Os:
+    private String os(){
+        String os = System.getProperty("os.name");
+        return os;
+    }
+    // Lista de processos
+    public void processos (){
+        if (os().contains("Windows")) {
+            try {
+                Process proc = Runtime.getRuntime().exec("TASKLIST /FO TABLE");
+                // ler os processos:
+                InputStream flux = proc.getInputStream();
+                InputStreamReader read = new InputStreamReader(flux);
+                BufferedReader leitor = new BufferedReader(read);
+                String line = leitor.readLine();
+                // Tratamento:
+                while (line != null){
+                    System.out.println(line);
+                    line = leitor.readLine();
+                }
+                // Fechar tudo:
+                flux.close();
+                leitor.close();
+                read.close();
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }
     }
 }
--- Exercicio02/src/view/Main.java
+++ Exercicio02/src/view/Main.java
@@ -1,7 +1,10 @@
 package view;
 
+import controller.KillController;
+
 public class Main {
     public static void main(String[] args) {
-        
+        KillController met = new KillController();
+        met.processos();
     }
 }
